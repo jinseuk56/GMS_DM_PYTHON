4c5447f64c1792398c76835754597f2fb7d5e1a0
RYU <35323569+jinseuk56@users.noreply.github.com>
2021-03-22 13:37:49 +0900

    updates

--- Tutorials for Beginners/load_mrc_ali_rec.py
+++ Tutorials for Beginners/load_mrc_ali_rec.py
@@ -1,8 +1,12 @@
 # Jinseok Ryu
 # Electron Microscopy and Spectroscopy Lab.
 # Seoul National University
-# last update : 20210110
+# last update : 20210322
 # load .mrc or .ali (tilt serise for electron tomography)
+# -> it needs "mrcfile" and "Hyperspy" (Python packages)
+# load .rec (reconstruction result from Inspect 3D)
+# -> it requires the shape of the reconstructed box
+# -> it is assumed that the data type of .rec files is 16-bit integer
 
 # ********************************************************************************
 print("Execute Python script in GMS 3")
@@ -19,8 +23,8 @@ print("Libraries have been imported completely")
 # ********************************************************************************
 
 if ( False == DM.IsScriptOnMainThread() ):
-	print('MatplotLib scripts require to be run on the main thread.')
-	exit()
+    print('MatplotLib scripts require to be run on the main thread.')
+    exit()
 
 file_adr = tkf.askopenfilename()
 print(file_adr)
@@ -34,6 +38,11 @@ if file_adr[-3:] == "ali":
     print(tilt_angles)
     tilt_rad = tilt_angles * np.pi / 180
     
+    data_dm = DM.CreateImage(data.copy())
+    data_dm.SetName("tilt series")
+    print(data_dm)
+    data_dm.ShowImage()
+    
 elif file_adr[-3:] == "mrc":
     mrc_file = mrcfile.open(file_adr)
     print(mrc_file)
@@ -45,9 +54,20 @@ elif file_adr[-3:] == "mrc":
     print("tilt angles (degree)")
     print(tilt_angles)
     tilt_rad = tilt_angles * np.pi / 180
-   
-data_dm = DM.CreateImage(data.copy())
-data_dm.SetName("tilt series")
-print(data_dm)
-
-data_dm.ShowImage()
+    
+    data_dm = DM.CreateImage(data.copy())
+    data_dm.SetName("tilt series")
+    print(data_dm)
+    data_dm.ShowImage()
+    
+elif file_adr[-3:] == "rec":
+    s1 = eval(input("1st dimensions (positive integer): "))
+    s2 = eval(input("2st dimensions (positive integer): "))
+    s3 = eval(input("3st dimensions (positive integer): "))
+    original_shape = (s1, s2, s3)
+    stack_tmp = np.fromfile(file_adr, dtype=np.int16)
+    orthoslices = stack_tmp[-np.prod(original_shape):].reshape(original_shape)
+    data_dm = DM.CreateImage(orthoslices.copy())
+    data_dm.SetName("orthoslices")
+    print(data_dm)
+    data_dm.ShowImage()
