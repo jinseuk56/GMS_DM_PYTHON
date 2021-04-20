5e8beb942de90f65fbed4fa0686220ecc4843bf1
RYU <35323569+jinseuk56@users.noreply.github.com>
2021-04-20 22:50:24 +0900

    Create load_tiff_stack.py

---
+++ Tutorials for Beginners/load_tiff_stack.py
@@ -0,0 +1,37 @@
+import sys
+sys.argv.extend(['-a', ' '])
+import DigitalMicrograph as DM
+import numpy as np
+import tifffile
+import tkinter.filedialog as tkf
+
+def threed_roll_axis(img):
+    stack = np.rollaxis(img, 2, 0)
+    return stack
+    
+def fourd_roll_axis(stack):
+    stack = np.rollaxis(np.rollaxis(stack, 2, 0), 3, 1)
+    return stack
+
+img_adr = tkf.askopenfilename()
+print(img_adr)
+
+
+if img_adr[-3:] == "tif" or img_adr[-4:]=="tiff":
+	data = tifffile.imread(img_adr)
+	print(data.shape)
+	
+else:
+	print("wrong input !")
+	exit()
+
+
+if len(data.shape) == 4:
+	data = fourd_roll_axis(data)
+
+elif len(data.shape) == 3:
+	data = threed_roll_axis(data)
+	
+data_dm = DM.CreateImage(data.copy())
+data_dm.SetName("tif file loaded")
+data_dm.ShowImage()
