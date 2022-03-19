7d00da4cb6d1328b3d8a78d8809412e817728955
RYU <35323569+jinseuk56@users.noreply.github.com>
2022-03-20 00:58:40 +0900

    Update load_tiff_stack.py

--- codes/load_tiff_stack.py
+++ codes/load_tiff_stack.py
@@ -18,23 +18,21 @@ print(img_adr)
 
 
 if img_adr[-3:] == "tif" or img_adr[-4:]=="tiff":
-	data = tifffile.imread(img_adr)
-	print(data.shape)
-	
+    data = tifffile.imread(img_adr)
+    print(data.shape)
+
 else:
-	print("wrong input !")
-	exit()
+    print("wrong input !")
+    exit()
 
 
 if len(data.shape) == 4:
-	data = fourd_roll_axis(data)
+    data = fourd_roll_axis(data)
 
-#elif len(data.shape) == 3:
-	#data = data.reshape(1, data.shape[0], data.shape[1], data.shape[2])
-	#data = fourd_roll_axis(data)
-	#data = threed_roll_axis(data)
-	
-	
+elif len(data.shape) == 3:
+    data = threed_roll_axis(data)
+
+
 data_dm = DM.CreateImage(data.copy())
 data_dm.SetName("tif file loaded")
 data_dm.ShowImage()
